d6ede5f7189e514d3c80da7927e9a448d3533313
yelyzaveta057 <lisa.rodionova2002@gmail.com>
2025-09-15 16:22:16 -0400

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { stripe } from "@/lib/stripe"
+import Link from "next/link";
 import Image from "next/image";
-import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+export default async function Home() {
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+    limit: 5,
+  });
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  return <div>
+    <section>
+    <div>
+      <div>
+        <h2>Welcome to me WebSite</h2>
+        <p> Discover the coolest pottery products at the best price.</p>
+        <Button asChild variant="default">
+          <Link href="/Products">Browse All Products </Link>
+        </Button>
+        </div> 
+        <Image alt="Banner Image" width={450} height={450} src={products.data[0].images[0]}/>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        </section>
+        </div>
 }
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,18 +1,21 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export const Navbar = () => {
-    return (
+  return (
     <nav className="sticky top-0 z-50 bg-white shadow">
-       
-        <div className="container mx-auto flex items-center justify-between px-4 py-4">
-            <Link href="/" className="hover:text-blue-600">My WebSite</Link>
-        </div>
-        <div className="hidden md:flex space-x-6">
-            <Link href="/">Home</Link>
+      <div className="max-w-7xl mx-auto px-4 py-4">
+        <div className="flex items-center justify-between">
+          <Link href="/" className="font-semibold hover:text-blue-600">
+            My Website
+          </Link>
+
+          <div className="hidden md:flex items-center space-x-6">
+            <Link href="/" className="hover:text-blue-600">Home</Link>
             <Link href="/Products" className="hover:text-blue-600">Products</Link>
             <Link href="/Checkout" className="hover:text-blue-600">Checkout</Link>
+          </div>
         </div>
-        <div className="flex items-center space-x-4"></div>
+      </div>
     </nav>
-    );
+  );
 };
